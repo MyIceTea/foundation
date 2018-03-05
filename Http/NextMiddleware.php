c605509946dba80b362ca3b20dda73fd522cde18
Ammar Faizi <ammarfaizi2@gmail.com>
2018-03-06 00:06:11 +0700

    Update c0d0cbf0bc1d3f077daf91f0da78e74d18cde5b1 2018-03-06 00:06:09

--- Http/NextMiddleware.php
+++ Http/NextMiddleware.php
@@ -2,15 +2,13 @@
 
 namespace EsTeh\Foundation\Http;
 
-/**
- * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
- * @package \EsTeh\Foundation\Http
- * @license MIT
- */
 class NextMiddleware
 {
-	public function __invoke()
+	public $next;
+
+	public function __invoke($next)
 	{
+		$this->next = $next;
 		return $this;
 	}
 }
